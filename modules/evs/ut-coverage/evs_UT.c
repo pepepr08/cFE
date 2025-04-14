aa279b12f2d8127508005240691b64d68a24f735
Jose F. Martinez Pedraza <jose.f.martinez-pedraza@nasa.gov>
2025-04-14 14:54:52 -0400

    Testing cast align flag in i686

--- modules/evs/ut-coverage/evs_UT.c
+++ modules/evs/ut-coverage/evs_UT.c
@@ -293,15 +293,10 @@ void Test_Init(void)
     CFE_EVS_EnableAppEventTypeCmd_t appbitcmd;
 
     CFE_SB_Buffer_t *bufPtr;
-    union 
-    {
-        CFE_SB_Buffer_t   SbBuf;
-        CFE_EVS_NoopCmd_t Noop;
-    } TestMsg;
-    
+    CFE_EVS_NoopCmd_t testMsg;
+
     UtPrintf("Begin Test Init");
 
-    memset(&TestMsg, 0, sizeof(TestMsg));
     memset(&bitmaskcmd, 0, sizeof(bitmaskcmd));
     memset(&appbitcmd, 0, sizeof(appbitcmd));
 
@@ -330,7 +325,7 @@ void Test_Init(void)
     /* Set unexpected message ID */
     UT_SetupBasicMsgDispatch(&UT_TPID_CFE_EVS_INVALID_MID, 0, true);
 
-    bufPtr = &TestMsg.SbBuf; /* Fake Test Message */
+    bufPtr = (CFE_SB_Buffer_t *)&testMsg; /* Fake Test Message */
     UT_SetDataBuffer(UT_KEY(CFE_SB_ReceiveBuffer), &bufPtr, sizeof(bufPtr), false);
 
     UT_EVS_DoGenericCheckEvents(CFE_EVS_TaskMain, &UT_EVS_EventBuf);
